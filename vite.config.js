65d358cf760ff57e86565f207ab64bcea56c53d4
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2026-05-29 23:05:52 +0500

    fx

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // <-- BARCHA ASSETLAR ROOTDAN OLINISHI UCHUN SHU QATORNI QO'SHING
   plugins: [
     react(),
     tailwindcss(),
